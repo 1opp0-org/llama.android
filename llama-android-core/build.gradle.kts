98c41beb22f1a58299d522154ff81bbeb3292d9c
Fabio de Matos <amazingappsemail@gmail.com>
2026-04-28 23:31:56 +1000

    Final clean up

--- llama-android-core/build.gradle.kts
+++ llama-android-core/build.gradle.kts
@@ -54,6 +54,12 @@ android {
 
                 if (llGuidanceEnabled) {
                     arguments += "-DLLAMA_LLGUIDANCE=ON"
+
+                    val rustupHome = System.getenv("RUSTUP_HOME")
+                    val cargoHome = System.getenv("CARGO_HOME")
+                    if ( rustupHome == null || cargoHome == null ) {
+                        throw RuntimeException("RUSTUP_HOME and/or CARGO_HOME environment variable are not set. These are required to build LLGuidance support with Rust. You can also disable LLGuidance by editing gradle.properties and setting llama.android.llguidance=false.")
+                    }
                 }
             }
         }
@@ -157,16 +163,10 @@ dependencies {
 }
 
 tasks.register("downloadTestModel") {
-//    val modelSmollUrl =
-//        "https://huggingface.co/HuggingFaceTB/SmolLM2-360M-Instruct-GGUF/resolve/main/smollm2-360m-instruct-q8_0.gguf"
 
-    val modelQwen_0_5B_q8_0_Url =
-        "https://huggingface.co/Qwen/Qwen2.5-0.5B-Instruct-GGUF/resolve/main/qwen2.5-0.5b-instruct-q8_0.gguf"
+    val modelQwen_0_5B_Url = "https://huggingface.co/Qwen/Qwen2.5-0.5B-Instruct-GGUF/resolve/main/qwen2.5-0.5b-instruct-q4_k_m.gguf"
 
-//    val modelQwen_0_5B_Url =
-//        "https://huggingface.co/Qwen/Qwen2.5-0.5B-Instruct-GGUF/resolve/main/qwen2.5-0.5b-instruct-q4_k_m.gguf?download=true"
-
-    val modelUrl = modelQwen_0_5B_q8_0_Url
+    val modelUrl = modelQwen_0_5B_Url
 
     val outputDir = file("src/androidTest/assets/")
     val outputFile = File(outputDir, "test_model.gguf")
--- llama-android-core/src/main/cpp/cargo-hijack.sh
+++ llama-android-core/src/main/cpp/cargo-hijack.sh
@@ -5,8 +5,16 @@
 # - RUST_NDK_TRIPLE
 # - ANDROID_NDK_CLANG_PATH
 # - NDK_BIN_DIR
+# It must also get the following ones somehow (usually from gradle's parent process environment):
+# - CARGO_HOME
+# - RUSTUP_HOME
 
-echo "Rust Build: Hijack script running for ${RUST_NDK_TRIPLE}"
+
+##############################
+# PREP AND CHECK ENVIRONMENT
+##############################
+
+echo "Rust Build: Hijack script running for Triple='${RUST_NDK_TRIPLE}'"
 
 # Calculate RUST_TARGET_UPPER (e.g., aarch64-linux-android -> AARCH64_LINUX_ANDROID)
 RUST_TARGET_UPPER=$(echo "${RUST_NDK_TRIPLE}" | tr '-' '_' | tr '[:lower:]' '[:upper:]')
@@ -14,15 +22,30 @@ RUST_TARGET_UPPER=$(echo "${RUST_NDK_TRIPLE}" | tr '-' '_' | tr '[:lower:]' '[:u
 export CARGO_TARGET_${RUST_TARGET_UPPER}_LINKER="${ANDROID_NDK_CLANG_PATH}"
 export CARGO_TARGET_${RUST_TARGET_UPPER}_AR="${NDK_BIN_DIR}/llvm-ar"
 
+if [ -z "${CARGO_HOME}" ] ; then
+  echo "Rust Build: CARGO_HOME is not set, fail build."
+  exit 1
+fi
+
+if [ -z "${RUSTUP_HOME}" ] ; then
+  echo "Rust Build: RUSTUP_HOME is not set, fail build."
+  exit 1
+fi
+
 # Check if a default toolchain is configured. If not, try to use 'stable'.
 if ! rustup default > /dev/null 2>&1; then
-    echo "Rust Build: No default toolchain found, attempting to use '+stable'"
-    CARGO_CMD="cargo +stable"
+    echo "Rust Build: No default toolchain found, fail build."
+    echo "CARGO_HOME  = ${CARGO_HOME}"
+    echo "RUSTUP_HOME = ${RUSTUP_HOME}"
+    exit 1
 else
     CARGO_CMD="cargo"
 fi
 
-# Run the real build
+##########
+# BUILD
+##########
+
 $CARGO_CMD build --release --package llguidance --target="${RUST_NDK_TRIPLE}"
 RES=$?
 
